55e09814af7032d3ab9d8e0fd339105fa3554e3a
Charlie Roberts <charliewoo1412@gmail.com>
2024-10-15 19:44:59 +0100

    Add some basic code to App.tsx to make the weather API call

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import { usePostcodeWeather } from "./hooks/useWeather";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [city, setCity] = useState("ME58RT");
+
+  const { data, error, isLoading } = usePostcodeWeather(city);
+
+  const handleCityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCity(e.target.value);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <h1>Weather App</h1>
+      <input
+        type="text"
+        value={city}
+        onChange={handleCityChange}
+        placeholder="Enter city name"
+      />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Error fetching weather data</p>
+      ) : data ? (
+        <div>
+          <h2>Name: {data.location.name}</h2>
+          <h2>Cloud: {data.current.cloud}</h2>
+        </div>
+      ) : (
+        <p>No data</p>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
