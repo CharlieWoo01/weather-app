54db46ffd987e662a60deaf3d40dcb0434eb3417
Charlie Roberts <charliewoo1412@gmail.com>
2024-10-15 21:20:12 +0100

    Use classnames for <Card/>

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import React from "react";
 
 interface CardProps {
@@ -7,10 +8,13 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({ title, children, className }) => {
+  const cardClasses = classNames(
+    "bg-gray-800 text-gray-400 font-bold rounded-lg shadow-lg p-6 w-full",
+    className
+  );
+
   return (
-    <div
-      className={`bg-gray-800 text-gray-400 font-bold rounded-lg shadow-lg p-6 w-full ${className}`}
-    >
+    <div className={cardClasses}>
       <h2 className="text-xs font-semibold mb-6 uppercase">{title}</h2>
       <div>{children}</div>
     </div>
