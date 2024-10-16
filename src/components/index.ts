18e56da85c6d623a2d49641368e43481d783f8d8
Charlie Roberts <charliewoo1412@gmail.com>
2024-10-16 23:40:36 +0100

    Add <Navigation/> component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Card } from './Card'
 export { default as Search } from './Search'
 export { default as WeatherToday } from './WeatherToday'
+export { default as Navigation } from './Navigation'
