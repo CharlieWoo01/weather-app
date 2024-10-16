8589b5c86c453124bb158d106589ee84a28fbda9
Charlie Roberts <charliewoo1412@gmail.com>
2024-10-16 20:01:52 +0100

    Refactor useWeather hook to use select to modify data instead of util

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,10 +1,45 @@
 import { useQuery } from "@tanstack/react-query";
-import { getWeatherByPostcode } from "../service/weatherService";
+import { getWeatherByPostcode, Hour } from "../service/weatherService";
+import { format } from "date-fns";
 
 export const usePostcodeWeather = (postcode: string, options = {}) => {
   return useQuery({
     ...options,
     queryKey: ["postcode", postcode],
     queryFn: () => getWeatherByPostcode(postcode),
+    select: (data) => {
+      const forecastDay = data?.forecast?.forecastday?.[0] ?? null;
+
+      // Return the original data if no forecast day is found
+      if (!forecastDay) {
+        return data;
+      }
+
+      const modifiedHourlyData =
+        forecastDay.hour?.map((hour: Hour) => {
+          const date = new Date(hour?.time ?? "");
+          const timeOnly = format(date, "HH:mm");
+
+          // Modify just the time with the formatted time
+          return {
+            ...hour,
+            time: timeOnly,
+          };
+        }) ?? [];
+
+      // Replace only the time field in the hourly data while preserving everything else
+      return {
+        ...data,
+        forecast: {
+          ...data.forecast,
+          forecastday: [
+            {
+              ...forecastDay,
+              hour: modifiedHourlyData,
+            },
+          ],
+        },
+      };
+    },
   });
 };
