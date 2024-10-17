25c95fcab4d0766341f73029d475717e336a8281
Charlie Roberts <charliewoo1412@gmail.com>
2024-10-17 22:47:57 +0100

    Redesign responsiveness of <WeatherToday/> component to handle screen sizes

--- src/components/WeatherToday/WeatherToday.tsx
+++ src/components/WeatherToday/WeatherToday.tsx
@@ -14,18 +14,16 @@ export default function WeatherToday({
   weatherAlt,
 }: WeatherTodayProps) {
   return (
-    <div className="w-full p-4 flex justify-center pt-20">
-      <div className="grid grid-cols-1 md:grid-cols-2 p-4 rounded-lg w-full max-w-2xl">
-        <div className="flex flex-col justify-between">
-          <div className="ml-8 mt-6">
-            <h1 className="text-white text-4xl font-semibold">{city}</h1>
-            <p className="text-gray-400 mt-1">Chance of cloud: {cloud}%</p>
-            <p className="text-5xl text-white font-bold mt-16">
-              {temperature}°
-            </p>
-          </div>
+    <div className="w-full p-4 flex justify-center ">
+      <div className="flex flex-col md:flex-row justify-around items-center rounded-lg w-full max-w-2xl">
+        <div className="flex flex-col justify-between items-center md:items-start md:pr-40">
+          <h1 className="text-white text-4xl font-semibold">{city}</h1>
+          <p className="text-gray-400 mt-1">Chance of cloud: {cloud}%</p>
+          <p className="text-5xl text-white font-bold mt-8 md:mt-16">
+            {temperature}°
+          </p>
         </div>
-        <div className="flex justify-center md:justify-end">
+        <div className="flex justify-center md:justify-end mt-8 md:mt-0">
           <img
             src={weatherImage}
             alt={weatherAlt}
