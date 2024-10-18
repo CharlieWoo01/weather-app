862b3c5907e001676cf8ac9d3418a6ec3e4f8969
Charlie Roberts <charliewoo1412@gmail.com>
2024-10-18 19:46:57 +0100

    Undo last commit and try trigger build again by making change

--- src/service/client.ts
+++ src/service/client.ts
@@ -1,10 +1,16 @@
 import axios from "axios";
 
-const client = axios.create({
-  baseURL: "https://api.weatherapi.com/v1",
+const clientConfig = {
+  baseURL: import.meta.env.VITE_WEATHER_API_BASE_URL,
   params: {
-    key: "",
+    key: import.meta.env.VITE_WEATHER_API_KEY,
   },
-});
+} as const;
+
+const client = axios.create(clientConfig);
 
 export default client;
+
+// todo: const as const object literal and array you can define it as this
+// todo: axe
+// todo: e2e testing for puppeteer? Performance and accessibility
