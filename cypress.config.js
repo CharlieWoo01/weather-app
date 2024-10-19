ad0e9aae8f3a1d1142891b418db05218b67d1b18
Charlie Roberts <charliewoo1412@gmail.com>
2024-10-19 22:04:08 +0100

    Setup and install Cypress with vite

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "cypress";
+import vitePreprocessor from "cypress-vite";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on) {
+      on("file:preprocessor", vitePreprocessor());
+    },
+    baseUrl: "http://localhost:5173/weather-app/",
+    specPattern: "cypress/e2e/**/*.cy.{js,ts}",
+  },
+});
