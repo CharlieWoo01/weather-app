30653f02c873925ad8a2a6cf01bcdd447110584f
Charlie Roberts <charliewoo1412@gmail.com>
2024-10-21 12:20:21 +0100

    Move query client config into it's own file

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 0,
-      refetchOnWindowFocus: false,
-      staleTime: 1000 * 60 * 1,
-    },
-  },
-});
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./queryClient.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
