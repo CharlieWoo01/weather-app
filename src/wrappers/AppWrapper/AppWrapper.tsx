0dbe53858ad3348cded68220ea52c493a0c1a57c
Charlie Roberts <charliewoo1412@gmail.com>
2024-10-21 11:50:32 +0100

    Use <Spinner/> component in <AppWrapper/> to handle global loading states

--- src/wrappers/AppWrapper/AppWrapper.tsx
+++ src/wrappers/AppWrapper/AppWrapper.tsx
@@ -1,14 +1,19 @@
 import { ReactNode } from "react";
 import { DesktopNavigation } from "../../components/Navigation/DesktopMenu";
 import { MobileNavigation } from "../../components/Navigation/MobileNavigation";
+import { Spinner } from "../../components";
+import { useIsFetching } from "@tanstack/react-query";
 
 interface AppWrapperProps {
   children: ReactNode | ReactNode[];
 }
 
 export function AppWrapper({ children }: AppWrapperProps) {
+  const isFetching = useIsFetching();
+
   return (
-    <>
+    <div>
+      {isFetching > 0 && <Spinner />}
       <div className="min-h-screen bg-gray-900 flex flex-col md:flex-row">
         {/* Desktop Navigation/medium screens and up) */}
         <div className="hidden md:flex md:flex-col md:w-20 md:h-screen md:bg-gray-800 md:fixed md:top-0 md:left-0 md:py-5">
@@ -21,6 +26,6 @@ export function AppWrapper({ children }: AppWrapperProps) {
           <MobileNavigation />
         </div>
       </div>
-    </>
+    </div>
   );
 }
