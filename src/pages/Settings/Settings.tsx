5007683a3e150890dbc27a601ef39575bef0bddc
Charlie Roberts <charliewoo1412@gmail.com>
2024-10-21 19:17:51 +0100

    Add basic layout for <Settings/> page

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -1,3 +1,13 @@
+import { Card } from "../../components";
+
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className="flex flex-col items-center w-full flex-grow md:ml-20 mb-16 md:mb-0">
+      <div className="w-full flex justify-center items-center my-4">
+        <Card title="Settings" className="w-full max-w-2xl text-center">
+          Test
+        </Card>
+      </div>
+    </div>
+  );
 }
