f68b0d12c9fbdc6f87999207a9c49267eebd038e
Charlie Roberts <charliewoo1412@gmail.com>
2024-10-21 11:52:29 +0100

    Tidy up <Home/> page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,7 @@ import { usePostcodeWeather } from "../../hooks/useWeather";
 import { Card, Search, WeatherToday } from "../../components";
 import classNames from "classnames";
 
-export default function EditProfile() {
+export default function Home() {
   const [city, setCity] = useState("London");
   const [searchTerm, setSearchTerm] = useState("London");
 
@@ -35,83 +35,66 @@ export default function EditProfile() {
   const location = weatherData?.location;
 
   return (
-    // <div className="min-h-screen bg-gray-900 flex flex-col md:flex-row">
-    //   {/* Desktop Navigation/medium screens and up) */}
-    //   <div className="hidden md:flex md:flex-col md:w-20 md:h-screen md:bg-gray-800 md:fixed md:top-0 md:left-0 md:py-5">
-    //     <DesktopNavigation />
-    //   </div>
-    <>
-      {/* Main content area */}
-      <div className="flex flex-col items-center w-full flex-grow md:ml-20 mb-16 md:mb-0">
-        {/* Search Bar */}
-        <div className="w-11/12 flex justify-center py-4">
-          <Search
-            onChange={handleCityChange}
-            onKeyDown={handleKeyDown}
-            value={city}
-            placeholder="Search for cities"
-            className="w-full max-w-2xl"
-          />
-        </div>
+    <div className="flex flex-col items-center w-full flex-grow md:ml-20 mb-16 md:mb-0">
+      {/* Search Bar */}
+      <div className="w-11/12 flex justify-center py-4">
+        <Search
+          onChange={handleCityChange}
+          onKeyDown={handleKeyDown}
+          value={city}
+          placeholder="Search for cities"
+          className="w-full max-w-2xl"
+        />
+      </div>
 
-        {/* Weather Today Component */}
-        <div className="w-full flex justify-center items-center my-4">
-          <WeatherToday
-            cloud={currentWeather?.cloud}
-            temperature={currentWeather?.temp_c}
-            weatherImage={currentWeather?.condition.icon}
-            weatherAlt={currentWeather?.condition.text}
-            city={location?.name}
-          />
-        </div>
+      {/* Weather Today Component */}
+      <div className="w-full flex justify-center items-center my-4">
+        <WeatherToday
+          cloud={currentWeather?.cloud}
+          temperature={currentWeather?.temp_c}
+          weatherImage={currentWeather?.condition.icon}
+          weatherAlt={currentWeather?.condition.text}
+          city={location?.name}
+        />
+      </div>
 
-        {/* Today's Forecast Card */}
-        <div className="w-full flex justify-center items-center my-4">
-          <Card
-            title="Today's Forecast"
-            className="w-full max-w-2xl text-center"
-          >
-            {isLoadingWeather ? (
-              <p className="text-gray-300">Loading...</p>
-            ) : paginatedHourlyTimes.length > 0 ? (
-              <div className="grid grid-cols-6 gap-2">
-                {paginatedHourlyTimes.map((weather, index) => (
-                  <div
-                    key={index}
-                    className={classNames("flex flex-col items-center pr-4", {
-                      "border-r-2 border-gray-700":
-                        index !== paginatedHourlyTimes.length - 1,
-                    })}
-                    data-testid={`forecast-item-${index}`}
+      {/* Today's Forecast Card */}
+      <div className="w-full flex justify-center items-center my-4">
+        <Card title="Today's Forecast" className="w-full max-w-2xl text-center">
+          {isLoadingWeather ? (
+            <p className="text-gray-300">Loading...</p>
+          ) : paginatedHourlyTimes.length > 0 ? (
+            <div className="grid grid-cols-6 gap-2">
+              {paginatedHourlyTimes.map((weather, index) => (
+                <div
+                  key={index}
+                  className={classNames("flex flex-col items-center pr-4", {
+                    "border-r-2 border-gray-700":
+                      index !== paginatedHourlyTimes.length - 1,
+                  })}
+                  data-testid={`forecast-item-${index}`}
+                >
+                  <p data-testid={`forecast-time-${index}`}>{weather.time}</p>
+                  <img
+                    src={weather.condition.icon}
+                    alt={`${weather.condition.text} icon`}
+                    className="w-16 h-16"
+                    data-testid={`forecast-icon-${index}`}
+                  />
+                  <p
+                    className="text-xl text-gray-300 mt-2"
+                    data-testid={`forecast-temperature-${index}`}
                   >
-                    <p data-testid={`forecast-time-${index}`}>{weather.time}</p>
-                    <img
-                      src={weather.condition.icon}
-                      alt={`${weather.condition.text} icon`}
-                      className="w-16 h-16"
-                      data-testid={`forecast-icon-${index}`}
-                    />
-                    <p
-                      className="text-xl text-gray-300 mt-2"
-                      data-testid={`forecast-temperature-${index}`}
-                    >
-                      {weather.temp_c}°
-                    </p>
-                  </div>
-                ))}
-              </div>
-            ) : (
-              <p className="text-gray-300">No data available</p>
-            )}
-          </Card>
-        </div>
+                    {weather.temp_c}°
+                  </p>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <p className="text-gray-300">No data available</p>
+          )}
+        </Card>
       </div>
-
-      {/* Mobile Navigation (visible on small screens) */}
-      {/* <div className="fixed bottom-0 left-0 w-full md:hidden">
-        <MobileNavigation />
-      </div>
-    </div> */}
-    </>
+    </div>
   );
 }
